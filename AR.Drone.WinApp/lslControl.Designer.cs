d7254bb5f1a8c7358f7b93f3447e668864209e04
jonas <jonas.broenstrup@gmail.com>
2014-12-01 12:56:30 +0100

    Erweiterte LSL-Kontrolle, alpha status,  nicht getestet.

--- AR.Drone.WinApp/lslControl.Designer.cs
+++ AR.Drone.WinApp/lslControl.Designer.cs
@@ -32,11 +32,16 @@
             this.start = new System.Windows.Forms.Button();
             this.lslTimer = new System.Windows.Forms.Timer(this.components);
             this.stop = new System.Windows.Forms.Button();
+            this.listStream = new System.Windows.Forms.ListBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.scaleFactor = new System.Windows.Forms.TextBox();
+            this.flymode = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.Location = new System.Drawing.Point(49, 41);
+            this.start.Enabled = false;
+            this.start.Location = new System.Drawing.Point(319, 297);
             this.start.Name = "start";
             this.start.Size = new System.Drawing.Size(75, 23);
             this.start.TabIndex = 0;
@@ -50,7 +55,8 @@
             // 
             // stop
             // 
-            this.stop.Location = new System.Drawing.Point(187, 41);
+            this.stop.Enabled = false;
+            this.stop.Location = new System.Drawing.Point(400, 297);
             this.stop.Name = "stop";
             this.stop.Size = new System.Drawing.Size(75, 23);
             this.stop.TabIndex = 1;
@@ -58,16 +64,64 @@
             this.stop.UseVisualStyleBackColor = true;
             this.stop.Click += new System.EventHandler(this.button2_Click);
             // 
+            // listStream
+            // 
+            this.listStream.FormattingEnabled = true;
+            this.listStream.ItemHeight = 16;
+            this.listStream.Location = new System.Drawing.Point(21, 12);
+            this.listStream.Name = "listStream";
+            this.listStream.Size = new System.Drawing.Size(211, 308);
+            this.listStream.TabIndex = 2;
+            this.listStream.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(238, 297);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 3;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // scaleFactor
+            // 
+            this.scaleFactor.Location = new System.Drawing.Point(253, 30);
+            this.scaleFactor.Name = "scaleFactor";
+            this.scaleFactor.Size = new System.Drawing.Size(121, 22);
+            this.scaleFactor.TabIndex = 4;
+            this.scaleFactor.Text = "10";
+            this.scaleFactor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.scaleFactor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // flymode
+            // 
+            this.flymode.FormattingEnabled = true;
+            this.flymode.Items.AddRange(new object[] {
+            "progressive",
+            "absolute"});
+            this.flymode.Location = new System.Drawing.Point(253, 72);
+            this.flymode.Name = "flymode";
+            this.flymode.Size = new System.Drawing.Size(121, 24);
+            this.flymode.TabIndex = 5;
+            this.flymode.Text = "progressive";
+            this.flymode.SelectedIndexChanged += new System.EventHandler(this.flymode_SelectedIndexChanged);
+            // 
             // lslControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 341);
+            this.ClientSize = new System.Drawing.Size(483, 343);
+            this.Controls.Add(this.flymode);
+            this.Controls.Add(this.scaleFactor);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.listStream);
             this.Controls.Add(this.stop);
             this.Controls.Add(this.start);
             this.Name = "lslControl";
             this.Text = "lslControl";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,5 +130,9 @@
         private System.Windows.Forms.Button start;
         private System.Windows.Forms.Timer lslTimer;
         private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.ListBox listStream;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.TextBox scaleFactor;
+        private System.Windows.Forms.ComboBox flymode;
     }
 }
--- AR.Drone.WinApp/lslControl.cs
+++ AR.Drone.WinApp/lslControl.cs
@@ -16,16 +16,23 @@ namespace AR.Drone.WinApp
         public liblsl.StreamInlet inlet;
         float[] sample = new float[1];
         private Drone.Client.DroneClient client;
+        private List<string> listItems = new List<string>();
+        private liblsl.StreamInfo[] results;
+        private float factor = 10;
 
         public lslControl(Drone.Client.DroneClient client)
         {
             InitializeComponent();
             // wait until an EEG stream shows up
-            liblsl.StreamInfo[] results = liblsl.resolve_stream("type", "EEG");
+            this.results = liblsl.resolve_stream("type", "EEG");
+            for (int i = 0; i < this.results.Count(); i++)
+            {
+                listItems.Add(this.results[i].name() + " (from " + this.results[i].hostname() + ")");
+            }
 
-            // open an inlet and print some interesting info about the stream (meta-data, etc.)
-            this.inlet = new liblsl.StreamInlet(results[0]);
-            System.Console.Write(inlet.info().as_xml());
+            listStream.DataSource = listItems;
+
+
 
             this.client = client;
             
@@ -34,22 +41,73 @@ namespace AR.Drone.WinApp
         private void button1_Click(object sender, EventArgs e)
         {
             Console.WriteLine("start");
+            float parsedText;
+            if (float.TryParse(scaleFactor.Text.Trim(), out parsedText))
+            {
+                this.factor = parsedText;
+                System.Console.WriteLine("Factor is " + this.factor);
+            }
+            else
+            {
+                MessageBox.Show("Please insert correct value for scaling factor.");
+            }
+
+            flymode.Enabled = false;
+            scaleFactor.Enabled = false;
+            stop.Enabled = true;
+            connect.Enabled = false;
             this.lslTimer.Start();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Console.WriteLine("stop");
             this.lslTimer.Stop();
+            connect.Enabled = true;
+            start.Enabled = false;
+            stop.Enabled = false;
+            flymode.Enabled = true;
+            scaleFactor.Enabled = true;
         }
 
         private void lslTimer_Tick(object sender, EventArgs e)
         {
             // read samples
             this.inlet.pull_sample(this.sample);
-            System.Console.WriteLine(this.sample);
+            System.Console.WriteLine(this.sample[0]);
+            if (flymode.SelectedIndex == 0)
+            {
+                client.Progress(FlightMode.Progressive, pitch: (sample.First() - 1) / this.factor);
+            }
+            else if (flymode.SelectedIndex == 1)
+            {
+                client.Progress(FlightMode.AbsoluteControl, pitch: (sample.First() - 1) / this.factor);
+            }
+        }
 
-            client.Progress(FlightMode.Progressive, pitch: (sample.First()-1)/10);
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // open an inlet and print some interesting info about the stream (meta-data, etc.)
+            this.inlet = new liblsl.StreamInlet(this.results[listStream.SelectedIndex]);
+            System.Console.Write(inlet.info().as_xml());
+            System.Console.WriteLine("Connected.");
+            start.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flymode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
